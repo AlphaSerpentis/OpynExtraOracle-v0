326ef027a5e137e45a64320c710da04f3f46f664
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2021-04-10 23:34:21 -0500

    Push changes

--- contracts/MultiPricer.sol
+++ contracts/MultiPricer.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.6.10;
 
+import {OracleInterface} from "./opyn/interfaces/OracleInterface.sol";
 import {OpynPricerInterface} from "./opyn/interfaces/OpynPricerInterface.sol";
 import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 
@@ -10,6 +11,7 @@ import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 /// @dev Pulls data from an oracle utilizing the OpynPricerInterface
 contract MultiPricer {
     using SafeMath for uint256;
+    using SafeMath for uint16;
 
     /// @dev Struct that stores price of an asset and timestamp of when it was recorded
     /// Adapted from GammaProtocol/contracts/Oracle.sol
@@ -18,18 +20,27 @@ contract MultiPricer {
         uint256 timestamp;
         address source; // Source of the price data
     }
+    /// @dev Struct that stores info for the Pricer
+    /// Weight can be used to determine the "trust" level
+    struct Pricer {
+        address source;
+        uint16 weight;
+        uint8 decimalsOfPrecision;
+    }
 
     /// @dev Permissioned addresses to pull price data from
-    /// (key = asset) -> (value = pricer)
-    mapping(address => address) internal pricers;
+    /// (key = asset) -> (value = pricers)
+    mapping(address => Pricer[]) internal pricers;
     /// @dev Permissioned addresses to allow to call functions
     mapping(address => bool) internal bots;
+    /// @dev Tolerable weight deviation
+    uint16 private tolerableWeightDeviation;
     /// @dev Opyn v2's oracle to submit price data to
-    address public opynOracle;
+    OracleInterface public opynOracle;
     /// @dev The admin can represent any multi-sig wallet capable of producing calls to the contract
     address private admin;
 
-    event PricerAdded(address asset, address pricer);
+    event PricerAdded(address asset, address pricer, uint16 trust);
     event PricerRemoved(address asset, address pricer);
     event BotAdded(address bot);
     event BotRemoved(address bot);
@@ -41,7 +52,7 @@ contract MultiPricer {
         address _opynOracle
     ) public {
         admin = _admin; // Make sure admin is a multi-sig wallet!
-        opynOracle = _opynOracle;
+        opynOracle = OracleInterface(_opynOracle);
     }
 
     modifier onlyBot {
@@ -58,17 +69,34 @@ contract MultiPricer {
     * @param _asset asset's address
     * @param _pricer pricer's address
     */
-    function addPricer(address _asset, address _pricer) external onlyAdmin {
-        pricers[_asset] = _pricer;
-        emit PricerAdded(_asset, _pricer);
+    function addPricer(
+        address _asset,
+        address _pricer,
+        uint16 _weight
+    )
+        external
+        onlyAdmin 
+    {
+        Pricer memory newPricer;
+        newPricer.source = _pricer;
+        newPricer.weight = _weight;
+
+        pricers[_asset].push(newPricer);
+        emit PricerAdded(_asset, _pricer, _weight);
     }
     /**
     * @notice Remove a pricer
     * @param _asset asset's address
     * @param _pricer pricer's address
     */
-    function removePricer(address _asset, address _pricer) external onlyAdmin {
-        pricers[_asset] = _pricer;
+    function removePricer(
+        address _asset,
+        address _pricer
+    )
+        external
+        onlyAdmin
+    {
+        delete pricers[_asset];
         emit PricerRemoved(_asset, _pricer);
     }
     /**
@@ -92,26 +120,60 @@ contract MultiPricer {
     * @param _newOracle new oracle's address for Opyn v2
     */
     function changeOpynOracle(address _newOracle) external onlyAdmin {
-        require(_newOracle != address(0), "ExtraOracle: Zero address");
-        opynOracle = _newOracle;
+        require(_newOracle != address(0), "MultiPricer: Zero address");
+        opynOracle = OracleInterface(_newOracle);
     }
     /**
     * @notice Change the contract's admin
     * @param _newAdmin new admin's address
     */
     function changeAdmin(address _newAdmin) external onlyAdmin {
-        require(_newAdmin != address(0), "ExtraOracle: Zero address");
+        require(_newAdmin != address(0), "MultiPricer: Zero address");
         admin = _newAdmin;
     }
     function pushData(address _asset) external onlyBot {
-        OpynPricerInterface pricer = OpynPricerInterface(pricers[_asset]);
+        
+        
 
     }
     function _onlyBot() internal view {
-        require(bots[msg.sender], "ExtraOracle: Unauthorized (bot)");
+        require(bots[msg.sender], "MultiPricer: Unauthorized (bot)");
     }
     function _onlyAdmin() internal view {
-        require(msg.sender == admin, "ExtraOracle: Unauthorized");
+        require(msg.sender == admin, "MultiPricer: Unauthorized");
     }
+    function _calculateWeightedAverage(
+        address _asset
+    )
+        internal
+        returns(uint256 weightedPrice)
+    {
+        // Check if there is even a pricer available
+        
 
+        // Check if the pricer was called recently, otherwise remove it from the calculation
+        
+        // Multiply the weights appropriately
+    }
+    function _reweighPricer(
+        Pricer[] storage _pricersToReweigh
+    )
+        internal
+        returns(Pricer[] memory pricers)
+    {
+
+    }
+    /**
+    * @notice Verify the weights did not deviate too far or is out of bounds
+    * @param _totalWeightValues total value of the weight 
+    */
+    function _weightDeviationToleranceCheck(
+        uint16 _totalWeightValues
+    )
+        internal
+        view
+    {
+        require(10000 - _totalWeightValues <= tolerableWeightDeviation, "MultiPricer: Intolerable weight deviation");
+        require(_totalWeightValues <= 10000, "MultiPricer: Illegal total weight value");
+    }
 }
